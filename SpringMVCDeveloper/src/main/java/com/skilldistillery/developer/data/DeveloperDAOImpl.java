1d0f6d233f9e0fe02d8a183eb9d3a42fc70a7ba6
newel <newel_21@yahoo.com>
2018-10-19 16:49:10 -0600

    Stub out DAO and DAOImpl

---
+++ SpringMVCDeveloper/src/main/java/com/skilldistillery/developer/data/DeveloperDAOImpl.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.developer.data;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.skilldistillery.developer.entities.Developer;
+
+@Transactional
+@Repository
+public class DeveloperDAOImpl implements DeveloperDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+
+	@Override
+	public Developer findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Developer> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Developer add(Developer dev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Developer update(Developer dev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Developer dev) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteById(int devId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
